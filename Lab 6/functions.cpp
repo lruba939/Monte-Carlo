c85767a5334e29e090eae71f7b440b7b6691fcf4
0ruba <lukaszruba939@gmail.com>
2024-04-13 00:48:21 +0200

    lab6 update

--- Lab 6/functions.cpp
+++ Lab 6/functions.cpp
@@ -29,9 +29,9 @@ void wiener(int N, double tmax, double dt, double D, std::fstream *pos, std::fst
     for(double t=0; t<=tmax; t+=dt){
         std::cout<<"Particles = "<<N<<" time = "<<t<<"\n";
 
-        std::vector<double> stat_values_x = {0,0,0}; // {mean, second moment, D_XX}
-        std::vector<double> stat_values_y = {0,0,0}; // {mean, second moment, D_YY}
-        std::vector<double> stat_values_xy = {0,0,0}; // {mean, second moment, D_XY}
+        std::vector<double> stat_values_x = {0,0,0,0,0,0}; // {mean, second moment, D_XX, meanD, secD, stdD}
+        std::vector<double> stat_values_y = {0,0,0,0,0,0}; // {mean, second moment, D_YY, meanD, secD, stdD}
+        std::vector<double> stat_values_xy = {0,0,0,0,0,0}; // {mean, second moment, D_XY, meanD, secD, stdD}
 
         for(int i=0; i<N; i++){
             normal_rand(dx, dy, sigma);
@@ -48,17 +48,23 @@ void wiener(int N, double tmax, double dt, double D, std::fstream *pos, std::fst
         }
         stat_values_xy[0] = stat_values_x[0]*stat_values_y[0]; // XY mean
 
-        if(t!=0){
-            stat_values_x[2] = (stat_values_x[1] - pow(stat_values_x[0],2))/(2.*t); // D_XX
-            stat_values_y[2] = (stat_values_y[1] - pow(stat_values_y[0],2))/(2.*t); // D_YY
-            stat_values_xy[2] = (stat_values_xy[1] - stat_values_xy[0])/(2.*t); // D_XY
-        }else{
-            stat_values_x[2] = NAN;
-            stat_values_y[2] = NAN;
-            stat_values_xy[2] = NAN;
-        }
+        stat_values_x[2] = (stat_values_x[1] - pow(stat_values_x[0],2))/(2.*(t+dt)); // D_XX
+        stat_values_y[2] = (stat_values_y[1] - pow(stat_values_y[0],2))/(2.*(t+dt)); // D_YY
+        stat_values_xy[2] = (stat_values_xy[1] - stat_values_xy[0])/(2.*(t+dt)); // D_XY
 
-        *stat<<N<<" "<<t<<" "<<stat_values_x[0]<<" "<<stat_values_y[0]<<" "<<stat_values_xy[0]<<" "<<stat_values_x[2]<<" "<<stat_values_y[2]<<" "<<stat_values_xy[2]<<"\n";
+        stat_values_x[3] += stat_values_x[2]/(t/dt+1); // mean D
+        stat_values_y[3] += stat_values_y[2]/(t/dt+1);
+        stat_values_xy[3] += stat_values_xy[2]/(t/dt+1);
+
+        stat_values_x[4] += pow(stat_values_x[2],2)/(t/dt+1); // second moment D
+        stat_values_y[4] += pow(stat_values_y[2],2)/(t/dt+1);
+        stat_values_xy[4] += pow(stat_values_xy[2],2)/(t/dt+1);
+
+        stat_values_x[5] = sqrt((stat_values_x[4] - pow(stat_values_x[3],2))/(t/dt+1)); // std D
+        stat_values_y[5] = sqrt((stat_values_y[4] - pow(stat_values_y[3],2))/(t/dt+1));
+        stat_values_xy[5] = sqrt((stat_values_xy[4] - pow(stat_values_xy[3],2))/(t/dt+1));
+        
+        *stat<<N<<" "<<t<<" "<<stat_values_x[0]<<" "<<stat_values_y[0]<<" "<<stat_values_xy[0]<<" "<<stat_values_x[2]<<" "<<stat_values_y[2]<<" "<<stat_values_xy[2]<<" "<<stat_values_x[5]<<" "<<stat_values_y[5]<<" "<<stat_values_xy[5]<<"\n";
 
         if(int(std::ceil(t*10))==int(std::ceil(tcondition*10))){
             std::cout<<"Done!\n";
@@ -86,7 +92,7 @@ void dyf_and_abs(int N, double tmax, double dt, double xs, double ys, double D, 
     for(double t=0; t<=tmax; t+=dt){
         int n = 0, nnew = 0;
 
-        double dn = omega * t;
+        double dn = omega * dt;
 
         std::vector<double> stat_values_x = {0,0,0}; // {mean, second moment, D_XX}
         std::vector<double> stat_values_y = {0,0,0}; // {mean, second moment, D_YY}
@@ -120,7 +126,7 @@ void dyf_and_abs(int N, double tmax, double dt, double xs, double ys, double D, 
                 pos_arr[0][i] = x2;
                 pos_arr[1][i] = y2;
 
-                if(pos_arr[2][i]){
+                if(pos_arr[2][i]==1){
                     n++;
                 }
             }
@@ -134,15 +140,10 @@ void dyf_and_abs(int N, double tmax, double dt, double xs, double ys, double D, 
         }
         stat_values_xy[0] = stat_values_x[0]*stat_values_y[0]; // XY mean
 
-        if(t!=0){
-            stat_values_x[2] = (stat_values_x[1] - pow(stat_values_x[0],2))/(2.*t); // D_XX
-            stat_values_y[2] = (stat_values_y[1] - pow(stat_values_y[0],2))/(2.*t); // D_YY
-            stat_values_xy[2] = (stat_values_xy[1] - stat_values_xy[0])/(2.*t); // D_XY
-        }else{
-            stat_values_x[2] = NAN;
-            stat_values_y[2] = NAN;
-            stat_values_xy[2] = NAN;
-        }
+        stat_values_x[2] = (stat_values_x[1] - pow(stat_values_x[0],2))/(2.*(t+dt)); // D_XX
+        stat_values_y[2] = (stat_values_y[1] - pow(stat_values_y[0],2))/(2.*(t+dt)); // D_YY
+        stat_values_xy[2] = (stat_values_xy[1] - stat_values_xy[0])/(2.*(t+dt)); // D_XY
+
         *stat<<N<<" "<<t<<" "<<stat_values_x[0]<<" "<<stat_values_y[0]<<" "<<stat_values_xy[0]<<" "<<stat_values_x[2]<<" "<<stat_values_y[2]<<" "<<stat_values_xy[2]<<" "<<n<<"\n";
 
         std::cout<<t<<" "<<t/tmax*100<<"%\n";
--- Lab 6/main.cpp
+++ Lab 6/main.cpp
@@ -12,31 +12,31 @@ int main(){
     double D=1;
     double xs = 0, ys = 0;
       
-    // std::fstream wiener_calc, wiener_stat;
-    // wiener_calc.open("wiener_pos.dat", std::fstream::out);
-    // wiener_stat.open("wiener_stat.dat", std::fstream::out);
-    //     wiener_stat<<"N t E_X E_Y E_XY D_XX D_YY D_XY\n";
+    std::fstream wiener_calc, wiener_stat;
+    wiener_calc.open("wiener_pos.dat", std::fstream::out);
+    wiener_stat.open("wiener_stat.dat", std::fstream::out);
+        wiener_stat<<"N t E_X E_Y E_XY D_XX D_YY D_XY\n";
 
-    // for(int N=pow(10,2); N<=pow(10,5); N*=10){
-    //     wiener(N, tmax, dt, D, &wiener_calc, &wiener_stat);
-    // }
-    // wiener_calc.close();
-    // wiener_stat.close();
+    for(int N=pow(10,2); N<=pow(10,5); N*=10){
+        wiener(N, tmax, dt, D, &wiener_calc, &wiener_stat);
+    }
+    wiener_calc.close();
+    wiener_stat.close();
     
-    D = 1;
-    double xr = 0, yr = 0, Rr = 5;
-    double xa = 3, ya = 0, Ra[2] = {0.1, 0.5};
-    xs = -4.5; ys = 0;
-    int Nmax = pow(10,4);
-    tmax = 1000; dt = 0.1;
-    double omega[3] = {10, 50, 100};
+    // D = 1;
+    // double xr = 0, yr = 0, Rr = 5;
+    // double xa = 3, ya = 0, Ra[2] = {0.1, 0.5};
+    // xs = -4.5; ys = 0;
+    // int Nmax = pow(10,4);
+    // tmax = 1000; dt = 0.1;
+    // double omega[3] = {10, 50, 100};
 
-    std::fstream da_pos, da_stat;
-    da_pos.open("omega0_Ra_1_pos.dat", std::fstream::out);
-    da_stat.open("omega0_Ra_1_stat.dat", std::fstream::out);
-        dyf_and_abs(Nmax, tmax, dt, xs, ys, D, omega[0], xr, yr, Rr, xa, ya, Ra[1], &da_pos, &da_stat);
-    da_pos.close();
-    da_stat.close();
+    // std::fstream da_pos, da_stat;
+    // da_pos.open("omega0_Ra_0_pos.dat", std::fstream::out);
+    // da_stat.open("omega0_Ra_0_stat.dat", std::fstream::out);
+    //     dyf_and_abs(Nmax, tmax, dt, xs, ys, D, omega[0], xr, yr, Rr, xa, ya, Ra[0], &da_pos, &da_stat);
+    // da_pos.close();
+    // da_stat.close();
 
     return 0;
 
